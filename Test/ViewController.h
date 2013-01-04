ec12ba049dc8807c81c69ab3a84c40c491ce4b54
Mithun Nair <nair50@avaya.com>
2013-01-04 12:21:48 +0530

    Revert "test"
    This reverts commit dfd5e7fa3622c3aedf0308975072d34d6b488e89.

--- Test/ViewController.h
+++ Test/ViewController.h
@@ -4,9 +4,8 @@
 //
 //  Created by Mithun Nair on 04/01/13.
 //  Copyright (c) 2013 Avaya. All rights reserved.
-//New comment
+//
 
-//Adding new comments
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
